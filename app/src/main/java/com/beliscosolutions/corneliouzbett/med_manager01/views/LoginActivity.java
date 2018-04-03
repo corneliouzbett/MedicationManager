7c68b20b41fe855986b584ccbcb92779fc4ab627
corneliouzBett <kbett68@gmail.com>
2018-04-03 13:33:00 +0300

    added login activity

---
+++ app/src/main/java/com/beliscosolutions/corneliouzbett/med_manager01/views/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.beliscosolutions.corneliouzbett.med_manager01.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
