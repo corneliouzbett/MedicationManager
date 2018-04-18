49cdb01edc5021897228b92fbb0229a31b0fc28c
corneliouzBett <kbett68@gmail.com>
2018-04-18 19:05:12 +0300

    refactored login

--- app/src/main/java/com/beliscosolutions/corneliouzbett/medmanager01/helpers/sql/DatabaseHelper.java
+++ app/src/main/java/com/beliscosolutions/corneliouzbett/medmanager01/helpers/sql/DatabaseHelper.java
@@ -178,11 +178,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         cursor.close();
         db.close();
 
-        if (cursorCount > 0) {
-            return true;
-        }
+        return cursorCount > 0;
 
-        return false;
     }
 
     /**
